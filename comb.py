8b98dbd48b1f68632bbd4095d87cd39df45fd064
Eric Karkos <ekarkos@cs.colostate.edu>
2018-04-30 12:38:19 -0600

    Finished loop which fills arrays w/ inital values.

--- comb.py
+++ comb.py
@@ -1,17 +1,26 @@
 import sys
 
 def comb(A,n,k,p,lo):
-  """
-    n>=1, k<=n, p: position to fill, lo: first number to pick
-    print all possible subsets of k out of n
-  """
+    poss = []
+    # Fill A[], and poss[], with possible integers out of k
+    for j in range(k):
+        poss.append(j)
+        A[j] = poss[j]
+
+    
+
+
+    """
+      n>=1, k<=n, p: position to fill, lo: first number to pick
+      print all possible subsets of k out of n
+    """
 
 if __name__ == "__main__":
-  d = len(sys.argv)>3
-  n = int(sys.argv[1])
-  k = int(sys.argv[2])
-  A = []
-  for i in range(k):
-      A.append(0)
-  if d: print("n:",n,"k:",k)
-  comb(A,n,k,0,0)
+    d = len(sys.argv)>3
+    n = int(sys.argv[1])
+    k = int(sys.argv[2])
+    A = []
+    for i in range(k):
+        A.append(0)
+    if d: print("n:",n,"k:",k)
+    comb(A,n,k,0,0)
