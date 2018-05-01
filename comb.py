cb8a96d1c7408d1427d37a6e65bfe73b95d8f4ff
Eric Karkos <ekarkos@cs.colostate.edu>
2018-05-01 11:43:37 -0600

    Continued work on comb recursive func.

--- comb.py
+++ comb.py
@@ -1,18 +1,18 @@
 import sys
 
 def comb(A,n,k,p,lo):
-    if p == len(A) or lo >= n:
+    if p >= len(A) or lo >= n:
         print(A)
         return
     else:
         A[p] = lo
+        comb(A, n, k, p, lo + 1)
         comb(A,n,k,p+1,lo+1)
-        comb(A,n,k,p,lo+1)
 
 if __name__ == "__main__":
     d = len(sys.argv)>3
-    n = int(5)
-    k = int(3)
+    n = int(sys.argv[1])
+    k = int(sys.argv[2])
     A = []
     for i in range(k):
         A.append(0)
