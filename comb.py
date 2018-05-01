beffc6dfd2717442617e1ffba1af433ec4771907
Eric Karkos <ekarkos@cs.colostate.edu>
2018-05-01 11:22:52 -0600

    general recursive algo finished, printing wrong output.

--- comb.py
+++ comb.py
@@ -1,19 +1,13 @@
 import sys
 
 def comb(A,n,k,p,lo):
-    if p >= len(A) or lo >= n:
+    if p == len(A) or lo >= n:
         print(A)
         return
     else:
         A[p] = lo
         comb(A,n,k,p+1,lo+1)
         comb(A,n,k,p,lo+1)
-
-
-    """
-      n>=1, k<=n, p: position to fill, lo: first number to pick
-      print all possible subsets of k out of n
-    """
 
 if __name__ == "__main__":
     d = len(sys.argv)>3
