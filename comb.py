fcde5c07db8751ca2b159ef7adbe619a3b800153
Eric Karkos <ekarkos@cs.colostate.edu>
2018-05-02 11:23:23 -0600

    Finished first attempt at recursive func.

--- comb.py
+++ comb.py
@@ -4,17 +4,17 @@ import sys
 # Place digits d from lo to hi in position p
 # Then recursively place digits in position p+1
 # lo: previously placed digit + 1
-# hi: n-k+p for pos p
+# hi: n-k+p for pos p (highest digit that can be in that pos)
 
 def comb(A,n,k,p,lo):
+    hi = (n-k)+p
     if p >= k or lo >= n:
         print(A)
     else:
-        # for i in range(p,k):
-        A[p] = lo
-        comb(A,n,k,p+1,lo+1)
-        A[p] = lo
-        comb(A,n,k,p,lo+1)
+        for i in range(lo,hi+1):
+            A[p] = i
+            comb(A,n,k,p+1,i+1)
+        #comb(A,n,k,p,lo+1)
 
 if __name__ == "__main__":
     d = len(sys.argv)>3
