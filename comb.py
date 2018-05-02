f1c6b34b7b1e551d950635dbe49fe2ed981804b3
Eric Karkos <ekarkos@cs.colostate.edu>
2018-05-02 11:38:12 -0600

    Finished and tested script, cleaned up formatting to fit python best practices.

--- comb.py
+++ comb.py
@@ -6,14 +6,15 @@ import sys
 # lo: previously placed digit + 1
 # hi: n-k+p for pos p (highest digit that can be in that pos)
 
-def comb(A,n,k,p,lo):
+
+def comb(A, n, k, p, lo):
     hi = (n-k)+p
     if p >= k or lo >= n:
         print(A)
     else:
-        for i in range(lo,hi+1):
+        for i in range(lo, hi+1):
             A[p] = i
-            comb(A,n,k,p+1,i+1)
+            comb(A, n, k, p+1, i+1)
 
 if __name__ == "__main__":
     d = len(sys.argv)>3
@@ -22,5 +23,5 @@ if __name__ == "__main__":
     A = []
     for i in range(k):
         A.append(0)
-    if d: print("n:",n,"k:",k)
-    comb(A,n,k,0,0)
+    if d: print("n:", n, "k:", k)
+    comb(A, n, k, 0, 0)
